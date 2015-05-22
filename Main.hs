aa01db3b04e554e5a5758687e9888668b0994751
Pascal Hartig <phartig@twitter.com>
2015-05-22 13:55:35 +0100

    Fall back to custom build

--- Main.hs
+++ Main.hs
@@ -1,4 +1,11 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text as T
+
+import qualified Rainbow as R
+import qualified Rainbow.Types as Y
 
 main :: IO ()
-main = return ()
+main = do
+    R.putChunkLn $ Y.chunkFromLazyText "Some blue text" & fore blue
+    putStrLn "Hi there."
