684943a121719f22da01097f44bdf0babde10edf
Pascal Hartig <phartig@twitter.com>
2015-06-18 11:40:20 +0100

    Set up stack

--- Main.hs
+++ Main.hs
@@ -1,8 +1,11 @@
 import Rainbow
+import System.Directory
+import Data.Foldable (forM_)
 import qualified Rainbow.Types as Y
-
-import Control.Lens
+import qualified System.FilePath.Find as Find
 
 main :: IO ()
 main = do
-    putChunkLn $ chunk "hello" & fore blue
+    dir <- getCurrentDirectory
+    files <- Find.find Find.always (Find.fileName Find.~~? "lint-results.xml") dir
+    forM_ files (\x -> putChunkLn $ chunk x & fore blue)
