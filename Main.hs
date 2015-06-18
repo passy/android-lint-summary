a406f3af0c249a580cb88fd2993b6e30a673dc58
Pascal Hartig <phartig@twitter.com>
2015-06-18 12:53:43 +0100

    Actually print some thing. This is easy!

--- Main.hs
+++ Main.hs
@@ -1,11 +1,22 @@
 import Rainbow
 import System.Directory
+import Text.HandsomeSoup
+import Text.XML.HXT.Core
+
 import Data.Foldable (forM_)
-import qualified Rainbow.Types as Y
+
 import qualified System.FilePath.Find as Find
+
+formatLintResults :: FilePath -> IO ()
+formatLintResults filename = do
+    contents <- readFile filename
+    let doc = readString [withWarnings yes] contents
+    links <- runX $ doc >>> css "issues issue" ! "summary"
+    forM_ links putStrLn
 
 main :: IO ()
 main = do
     dir <- getCurrentDirectory
     files <- Find.find Find.always (Find.fileName Find.~~? "lint-results.xml") dir
-    forM_ files (\x -> putChunkLn $ chunk x & fore blue)
+    forM_ files formatLintResults
+    -- forM_ files (\x -> putChunkLn $ chunk x & fore blue)
